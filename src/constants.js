4c00be71d999c51fc67fb26d1cc22a2fab725189
Adarshsahoo1703 <adarsh.kumar2020@vitbhopal.ac.in>
2025-05-19 00:00:55 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,309 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import Cypress from './assets/tech_logo/Cypress.png';
+import Gerrit from './assets/tech_logo/Gerrit.png'
+import Putty from './assets/tech_logo/Putty.png'
+import Swagger from './assets/tech_logo/Swagger.png'
+import Tensorflow from './assets/tech_logo/Tenserflow.png'
+import Keras from './assets/tech_logo/Keras.png'
+import Scikit from './assets/tech_logo/Scikitlearn.png'
+import Pandass from './assets/tech_logo/Pandas.png'
+import Pytorch from './assets/tech_logo/Pytorch.png'
+import Numpylogo from './assets/tech_logo/Numpy.png'
+import Dockerlogo from './assets/tech_logo/Docker.png'
+import Kuberneteslogo from './assets/tech_logo/Kubernetes.png'
+
+// Experience Section Logo's
+import NokiaLogo from './assets/company_logo/Nokia.png'
+import Satcomlogo from './assets/company_logo/Satcom.png'
+
+// Education Section Logo's
+import VitLogo from './assets/education_logo/VIT.png'
+import Narayanalogo from './assets/education_logo/Narayana.png'
+import Calpslogo from './assets/education_logo/Calps.png'
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+import cnnlogo from './assets/work_logo/cnn.png'
+import signlogo from './assets/work_logo/Signtotext.png'
+import Envlogo from './assets/work_logo/Envweb.png'
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Cypress', logo: Cypress },
+      { name: 'Gerrit', logo: Gerrit },
+      { name: 'Putty', logo: Putty },
+      { name: 'Swagger', logo: Swagger },
+      { name: 'Docker', logo: Dockerlogo },
+      { name: 'Kubernetes', logo: Kuberneteslogo },
+    ],
+  },
+    {
+    title: 'AL/ML',
+    skills: [
+      { name: 'TenserFlow', logo: Tensorflow },
+      { name: 'Keras', logo: Keras },
+      { name: 'Pytorch', logo: Pytorch },
+      { name: 'Scikit-Learn', logo: Scikit },
+      { name: 'Panda', logo: Pandass },
+      { name: 'Numpy', logo: Numpylogo },
+
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: NokiaLogo,
+      role: "SDE Intern",
+      company: "Nokia solutions And Network Pvt Ltd",
+      date: "August 2024 - Present",
+      desc: "I played a key role in developing a cloud femtocell console management system, optimizing both UI/UX to improve ease of use for administrators. Using React.js, I designed and implemented new features for the web-based console, enhancing responsiveness and security. I also automated UI testing with Cypress to ensure high test coverage and fast feedback. Additionally, I worked with Docker for containerization and Kubernetes for orchestration, ensuring scalable and high-availability deployments. Collaborating closely with the backend team, I helped integrate features, improve API interactions, and participated in sprint planning, code reviews, and agile ceremonies for timely feature delivery.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MariaDb",
+        "Redux",
+        "Cypress",
+        "ReactJest",
+        "Docker",
+        "Kubernetes",
+        "Gerrit",
+        "GIT",
+        "JIRA",
+      ],
+    },
+    {
+      id: 1,
+      img: Satcomlogo,
+      role: " Data And Security Analyst Intern",
+      company: "Satcom & Digital Solutions Pvt. Ltd.",
+      date: "January 2024 - March 2024",
+      desc: " Monitored and analyzed network traffic to detect potential security threats and vulnerabilities, ensuring the integrity and security of organizational data. Implemented and managed firewalls, intrusion detection/prevention systems, and VPNs to safeguard network infrastructure against unauthorized access. Conducted regular security audits and risk assessments to identify weaknesses in network architecture and ensure compliance with industry standards and regulations.",
+      skills: [
+        "NAC",
+        "VPN",
+        "Fortinet",
+        "OpenVAS",
+        "NAS",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: VitLogo,
+      school: "Vellore Institute of Technology, Bhopal",
+      date: "Sept 2020 - May 2025",
+      grade: "8.82 CGPA",
+      desc: "  Currently Pursuing Final year Computer Science student at Vellore Institute of Technology with a specialization in Artificial Intelligence",
+      degree: "Integrated MTech In AI - MTech",
+    },
+    {
+      id: 1,
+      img: Narayanalogo,
+      school: "Narayana Jr College, Hyderabad",
+      date: "June 2018 - Mar 2020",
+      grade: "86%",
+      desc: "I completed my class 12 education from Narayana Jr College, Hyderabad, under the SSE board, where I studied Physics, Chemistry, and Mathematics (PCM).",
+      degree: "12th",
+    },
+    {
+      id: 2,
+      img: Calpslogo,
+      school: "CALPS, Hyderabad",
+      date: "March 2018",
+      grade: "84%",
+      desc: "I completed my class 10 education from CALPS, Hyderabad , under the CBSE board.",
+      degree: "10th",
+    },
+  
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: " CNN Based Food Scanning Model for Calorie Analysis",
+      description:
+        "Developed a CNN-based model that scans food images to predict food items and perform a detailed calorie analysis. The model uses deep learning techniques to identify food types and estimate their nutritional values, offering real-time feedback on food intake",
+      image: cnnlogo,
+      tags: ["Python", "TensorFlow", "CNN", "OpenCV", "Pandas"],
+      github: "https://github.com/Adarshsahoo1703",
+      webapp: "https://ieeexplore.ieee.org/document/10507809",
+    },
+    {
+      id: 1,
+      title: " Sign Language To Text Converter",
+      description:
+        "Created an application that uses computer vision and machine learning to convert sign language gestures into English text in real-time. The system uses MediaPipe for hand tracking and OpenCV for gesture recognition.",
+      image: signlogo,
+      tags: ["Python", "Media-Pipe", "OpenCV"],
+      github: "https://github.com/Adarshsahoo1703",
+      webapp: "https://github.com/Adarshsahoo1703",
+    },
+    {
+      id: 2,
+      title: "Envision.AI",
+      description:
+        "Developed and Deployed ML Model for Predicting Second-Hand Car and House Prices Created a machine learning model that predicts the price of second-hand cars and houses based on historical data. The model was integrated into a web application to provide users with real-time price estimations.",
+      image: Envlogo,
+      tags: ["HTML", "CSS", "WordPress", "Python","ML","Flask"],
+      github: "https://github.com/Adarshsahoo1703",
+      webapp: "https://github.com/Adarshsahoo1703",
+    },
+    // {
+    //   id: 3,
+    //   title: "Email Validator NPM Package",
+    //   description:
+    //     "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+    //   image: npmLogo,
+    //   tags: ["React JS", "Node.js", "NPM", "Validation"],
+    //   github: "https://github.com/codingmastr/cmtk-email-validator",
+    //   webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Task Reminder Chrome Extension Tool",
+    //   description:
+    //     "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    //   image: taskremLogo,
+    //   tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    //   github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    //   webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Webverse Digital",
+    //   description:
+    //     "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    //   image: webverLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    //   github: "https://github.com/codingmastr/Webverse-Digital",
+    //   webapp: "https://webversedigital.com/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
